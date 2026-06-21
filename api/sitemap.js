196a182ab05ddde484f317b3ebb0895a9084f124
alitang618-a11y <alitang618@gmail.com>
2026-06-22 03:23:56 +0800

    Create sitemap.js

---
+++ api/sitemap.js
@@ -0,0 +1,45 @@
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler() {
+  const baseUrl = "https://xiedang.com";
+  const staticPages = [
+    { path: "/", priority: "1.0", changefreq: "daily" },
+  ];
+
+  const topics = [
+    "2026-sci-fi",
+    "2025-horror",
+    "top250-movie",
+    "independent-film",
+    "new-upcoming-2026",
+    "top-animation-tv"
+  ];
+  const topicUrls = topics.map(key => ({
+    path: `/?topic=${key}`,
+    priority: "0.8",
+    changefreq: "weekly"
+  }));
+
+  const allUrls = [...staticPages, ...topicUrls];
+
+  // 纯XML字符串，无多余隐藏字符
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+  allUrls.forEach(item => {
+    xml += `
+  <url>
+    <loc>${baseUrl}${item.path}</loc>
+    <changefreq>${item.changefreq}</changefreq>
+    <priority>${item.priority}</priority>
+  </url>`;
+  });
+  xml += "\n</urlset>";
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "s-maxage=3600",
+    },
+  });
+}
